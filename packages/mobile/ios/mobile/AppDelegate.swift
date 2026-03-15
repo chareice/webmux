1c984499957cc6e167128442f7cd697413dc3154
ChengLei Shao <chareice@live.com>
2026-03-15 18:14:01 +0800

    chore: set production mobile app identifiers

--- packages/mobile/ios/mobile/AppDelegate.swift
+++ packages/mobile/ios/mobile/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "mobile",
+      withModuleName: "webmux",
       in: window,
       launchOptions: launchOptions
     )
